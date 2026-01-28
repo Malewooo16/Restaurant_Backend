e924af972a738145c21e3e0057a518a7b4192542
MalewooooDev <elijahmalewo16@gmail.com>
2026-01-29 00:00:36 +0300

    Updated Cors V12

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ app.use('/api-docs', swaggerRoutes);
 app.use('/api', routes);
 
 app.use('/health', (req, res) => {
-  res.status(200).send('OK Server is healthy v11');
+  res.status(200).send('OK Server is healthy v12');
 });
 
 app.listen(port, () => {
