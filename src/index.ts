af3a60b7a0bf0929dce74cb96b555ebd1606153c
MalewooooDev <elijahmalewo16@gmail.com>
2026-01-30 10:16:15 +0300

    Updated Table and Reservation Routes v17

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ app.use('/api-docs', swaggerRoutes);
 app.use('/api', routes);
 
 app.use('/health', (req, res) => {
-  res.status(200).send('OK Server is healthy v16');
+  res.status(200).send('OK Server is healthy v17');
 });
 
 app.listen(port, () => {
