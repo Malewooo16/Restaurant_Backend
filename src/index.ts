1dc35897622d4584b91546795976ec9f819fd130
MalewooooDev <elijahmalewo16@gmail.com>
2026-02-05 13:42:13 +0300

    Updated CORS v6

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import express from 'express';
-import routes from './modules/routes';
-import swaggerRoutes from './swagger';
-import cors from 'cors';
+import express from "express";
+import routes from "./modules/routes";
+import swaggerRoutes from "./swagger";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,24 +9,21 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
   "http://localhost:8080",
-  "https://restaurant.maison.run.place.",
-  "http://212.115.110.115:8080",  //Swagger server
-  "http://tauri.localhost"
+  "https://restaurant.maison.run.place",
+  "http://212.115.110.115:8080", //Swagger server
+  "http://tauri.localhost",
 ];
-
-
-app.use(express.json());
-
 
 app.use(
   cors({
     origin: (origin, callback) => {
-      // allow server-to-server / curl / Postman
+      console.log("Incoming Request Origin:", origin); // Add this line
       if (!origin) return callback(null, true);
 
       if (allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
+        console.error(`CORS Blocked: ${origin} is not in allowedOrigins`);
         callback(new Error("Not allowed by CORS"));
       }
     },
@@ -34,11 +31,14 @@ app.use(
     credentials: true,
   })
 );
-app.use('/api-docs', swaggerRoutes);
-app.use('/api', routes);
 
-app.use('/health', (req, res) => {
-  res.status(200).send('OK Server is healthy v19');
+app.use(express.json());
+
+app.use("/api-docs", swaggerRoutes);
+app.use("/api", routes);
+
+app.use("/health", (req, res) => {
+  res.status(200).send("OK Server is healthy v21");
 });
 
 app.listen(port, () => {
